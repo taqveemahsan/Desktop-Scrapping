9662a1fd43da04e6617eabfadf7985f8e4fa15f6
Taqveem Ahsan <taqi.malik86@gmail.com>
2021-01-29 15:11:10 +0500

    Add project files.

---
+++ Desktop Scrapping/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿
+namespace Desktop_Scrapping
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelMenu = new System.Windows.Forms.Panel();
+            this.panelLogo = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panelTop = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panelMenu.SuspendLayout();
+            this.panelLogo.SuspendLayout();
+            this.panelTop.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelMenu
+            // 
+            this.panelMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(50)))), ((int)(((byte)(57)))));
+            this.panelMenu.Controls.Add(this.panelLogo);
+            this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelMenu.Name = "panelMenu";
+            this.panelMenu.Size = new System.Drawing.Size(205, 502);
+            this.panelMenu.TabIndex = 0;
+            // 
+            // panelLogo
+            // 
+            this.panelLogo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(16)))), ((int)(((byte)(19)))));
+            this.panelLogo.Controls.Add(this.label1);
+            this.panelLogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelLogo.Location = new System.Drawing.Point(0, 0);
+            this.panelLogo.Name = "panelLogo";
+            this.panelLogo.Size = new System.Drawing.Size(205, 80);
+            this.panelLogo.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MetropolitainesD", 24F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(6, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(193, 35);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Scrap Data";
+            // 
+            // panelTop
+            // 
+            this.panelTop.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(61)))), ((int)(((byte)(68)))), ((int)(((byte)(53)))));
+            this.panelTop.Controls.Add(this.button1);
+            this.panelTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTop.Location = new System.Drawing.Point(205, 0);
+            this.panelTop.Name = "panelTop";
+            this.panelTop.Size = new System.Drawing.Size(576, 80);
+            this.panelTop.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(466, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Export PDF";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(312, 224);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(134, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Enter Minimum Sold Items: \r\n";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(312, 189);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Enter Item Name: ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.AllowDrop = true;
+            this.textBox1.Location = new System.Drawing.Point(452, 186);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(179, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(452, 221);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(179, 20);
+            this.textBox2.TabIndex = 8;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.LightGreen;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Malache Crunch", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(547, 305);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(84, 38);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Scrap";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(452, 257);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(179, 20);
+            this.textBox3.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(312, 260);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(109, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Enter Minimum Price: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(781, 502);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panelTop);
+            this.Controls.Add(this.panelMenu);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panelMenu.ResumeLayout(false);
+            this.panelLogo.ResumeLayout(false);
+            this.panelLogo.PerformLayout();
+            this.panelTop.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelMenu;
+        private System.Windows.Forms.Panel panelLogo;
+        private System.Windows.Forms.Panel panelTop;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Desktop Scrapping/Form1.cs
@@ -0,0 +1,257 @@
+﻿using Desktop_Scrapping.Model;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium.Interactions;
+
+namespace Desktop_Scrapping
+{
+    public partial class Form1 : Form
+    {
+        public IWebDriver driver;
+        ScrapeTestEntities model = new ScrapeTestEntities();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var Item_name = textBox1.Text;
+            var Sold_items = textBox2.Text;
+            var item_pricce = textBox3.Text;
+            ScrapeData(Item_name, Sold_items);
+        }
+        public void ScrapeData(string Search_Value = "slicer", string Search_sold = "500")
+        {
+            try
+            {
+                //Declaration of Variables
+                var Message = "";
+                ItemsName items = new ItemsName();
+                SellerNameScrape sellerNameScrape = new SellerNameScrape();
+                List<string> NameProducts = new List<string>();
+                List<string> sellerName = new List<string>();
+                IList<IWebElement> namesList;
+                IList<IWebElement> ItemSList;
+                IList<IWebElement> location;
+                IList<IWebElement> SellerList;
+                IList<IWebElement> soldList;
+
+                //Open CHromium and Get First Values
+                IWebDriver driver = new ChromeDriver(@"c:/");
+                driver.Url = "https://www.aliexpress.com/";
+                driver.Manage().Window.Minimize();
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+
+                //Initialize IWEBELEMENTS variables
+                driver.FindElement(By.Id("search-key")).SendKeys(Search_Value + OpenQA.Selenium.Keys.Enter);
+                ((IJavaScriptExecutor)driver).ExecuteScript("window.scrollTo(0,1080)");
+                namesList = driver.FindElements(By.ClassName("item-title"));
+                soldList = driver.FindElements(By.XPath("//*[@id='root']/div/div/div[2]/div[2]/div/div[2]/ul/div/li/div/div/div/div/div/span/a"));
+
+                //*[@id="root"]/div/div/div[2]/div[2]/div/div[2]/ul/div[2]/li[1]/div/div[2]/div/div[7]/div/span/a
+                ((IJavaScriptExecutor)driver).ExecuteScript("window.scrollTo(0,1080)");
+                var numb = 1;
+                var listNumber = 1;
+                ((IJavaScriptExecutor)driver).ExecuteScript("window.scrollTo(0,1080)");
+
+                if (soldList.Count != 0)
+                {
+                    //Select all Values of Sold List Items
+                    for (int i = 0; i < namesList.Count; i++)
+                    {
+                        driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+                        var curr = ((IJavaScriptExecutor)driver).ExecuteScript("return window.pageYOffset;").ToString();
+                        var curr1 = Int32.Parse(curr);
+                        ((IJavaScriptExecutor)driver).ExecuteScript("window.scrollTo(" + curr1 + "," + (curr1 + 900) + ")");
+                        Console.WriteLine(namesList[i].Text);
+                        namesList = driver.FindElements(By.ClassName("item-title"));
+                        soldList = driver.FindElements(By.XPath("//*[@id='root']/div/div/div[2]/div[2]/div/div[2]/ul/div/li/div/div/div/div/div/span/a"));
+                    }
+                    ((IJavaScriptExecutor)driver).ExecuteScript("window.scrollTo(0,1080)");
+                    //Check first Condition if user add sold item value and get all items list from Ali Express
+                    for (int s = 0; s < soldList.Count; s++)
+                    {
+                        var curr = ((IJavaScriptExecutor)driver).ExecuteScript("return window.pageYOffset;").ToString();
+                        var curr1 = Int32.Parse(curr);
+                        ((IJavaScriptExecutor)driver).ExecuteScript("window.scrollTo(" + curr1 + "," + (curr1 + 900) + ")");
+                        //Console.WriteLine(namesList[i].Text);
+                        //soldList = driver.FindElements(By.XPath("sale-value-link"));
+                        var soldValue = soldList[s].Text;
+                        var soldValue1 = soldValue.ToString();
+
+                        //Find Number from string
+                        string b = string.Empty;
+                        var intValueSold = 1.00;
+
+                        for (int i = 0; i < soldValue1.Length; i++)
+                        {
+                            if (Char.IsDigit(soldValue1[i]))
+                                b += soldValue1[i];
+                        }
+
+                        if (b.Length > 0)
+                            intValueSold = Int32.Parse(b);
+
+                        var ValueFromInput = Int32.Parse(Search_sold);
+
+                        //ValueFromInput given by User
+                        if (intValueSold >= ValueFromInput)
+                        {
+                            if (listNumber > 4)
+                            {
+                                numb++;
+                                listNumber = 1;
+
+                            }
+                            //Get specific element name given by user
+                            IWebElement ValueName = driver.FindElement(By.XPath("//*[@id='root']/div/div/div[2]/div[2]/div/div[2]/ul/div[" + numb + "]/li[" + listNumber + "]/div/div[2]/div/div[1]/a"));
+                            numb = (s / 4) + 1;
+
+                            var listLength = namesList.Count;
+                            //namesList.Append(ValueName);
+
+                            //Save names in List Items of string 
+                            NameProducts.Add(ValueName.Text);
+                            items.ItemsName1 = ValueName.Text;
+
+                            //Save all Item names in DataBase
+                            model.ItemsNames.Add(items);
+                            model.SaveChanges();
+                            listNumber++;
+
+                        }
+                    }
+
+
+
+
+                    //*[@id="root"]/div/div/div[2]/div[2]/div/div[2]/ul/div[1]/li[1]/div/div[2]/div/div[1]/a
+                    //*[@id="root"]/div/div/div[2]/div[2]/div/div[2]/ul/div[1]/li[2]/div/div[2]/div/div[1]/a
+                    //*[@id="root"]/div/div/div[2]/div[2]/div/div[2]/ul/div[2]/li[1]/div/div[2]/div/div[1]/a
+                    //*[@id="root"]/div/div/div[2]/div[2]/div/div[2]/ul/div[3]/li[1]/div/div[2]/div/div[1]/a
+                    //*[@id="root"]/div/div/div[2]/div[2]/div/div[2]/ul/div[15]/li[1]/div/div[2]/div/div[1]/a
+
+
+                    //Check values of all getting values
+                    var total = namesList.Count;
+                    var totalSolids = soldList.Count;
+                    var listCount = NameProducts.Count;
+
+                    //Start chromium for Ebay
+                    IWebDriver driver1 = new ChromeDriver(@"c:/");
+                    driver1.Url = "https://www.ebay.com/";
+                    driver1.Manage().Window.Minimize();
+                    driver1.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+                    if (listCount != 0)
+                    {
+                        //Search and Gets all Products one by one from list
+                        for (int i = 0; i < NameProducts.Count; i++)
+                        {
+                            //var name = namesList[i].Text;
+                            var name = NameProducts[i].ToString();
+                            var WordsArray = name.Split();
+                            string Items = WordsArray[0] + ' ' + WordsArray[1] + ' ' + WordsArray[2];
+                            //IEnumerable<string> words = name.Split(new char[] { ' ' }, 2).Take(5);
+                            driver1.FindElement(By.Id("gh-ac")).SendKeys(Items + OpenQA.Selenium.Keys.Enter);
+                            location = driver1.FindElements(By.ClassName("s-item__location"));
+                            //Gets locations of the values given in products search
+                            for (int j = 0; j < location.Count; j++)
+                            {
+                                var curr = ((IJavaScriptExecutor)driver).ExecuteScript("return window.pageYOffset;").ToString();
+                                var curr1 = Int32.Parse(curr);
+                                ((IJavaScriptExecutor)driver).ExecuteScript("window.scrollTo(" + curr1 + "," + (curr1 + 900) + ")");
+                                Console.WriteLine(location[j].Text);
+                                location = driver1.FindElements(By.ClassName("s-item__location"));
+
+                            }
+                            var locationItem = location.Count;
+                            //Check locations value 
+                            for (int k = 0; k < location.Count; k++)
+                            {
+                                var curr = ((IJavaScriptExecutor)driver).ExecuteScript("return window.pageYOffset;").ToString();
+                                var curr1 = Int32.Parse(curr);
+                                ((IJavaScriptExecutor)driver).ExecuteScript("window.scrollTo(" + curr1 + "," + (curr1 + 900) + ")");
+                                //Console.WriteLine(location[k].Text);
+                                location = driver1.FindElements(By.ClassName("s-item__location"));
+                                var locationName = location[k].Text;
+                                locationName.ToString();
+                                var num = k + 1;
+                                if (locationName == "From China" || locationName == "De China")
+                                {
+                                    try
+                                    {
+                                        driver1.FindElement(By.XPath("//*[@id='srp-river-results']/ul/li[" + num + "]/div/div[2]/a")).Click();
+                                        driver1.FindElement(By.ClassName("mbg-nw")).Click();
+                                        //*[@id="RightSummaryPanel"]/div[3]/div/div/div/div[1]/div[1]/a
+                                        var locationOfSeller = driver1.FindElement(By.ClassName("mem_loc"));
+                                        var TextSeller = locationOfSeller.Text;
+                                        var locationOfSellers = TextSeller.ToString();
+
+                                        //Check seller is not from China
+                                        if (locationOfSellers != "China")
+                                        {
+                                            IWebElement SellerName1 = driver1.FindElement(By.ClassName("mbg-id"));
+                                            sellerName.Add(SellerName1.Text);
+                                            sellerNameScrape.SellerName = SellerName1.Text;
+
+                                            //Add Seler name in DataBase
+                                            model.SellerNameScrapes.Add(sellerNameScrape);
+                                            model.SaveChanges();
+
+                                        }
+                                        driver1.Navigate().Back();
+                                        driver1.Navigate().Back();
+                                    }
+                                    catch (Exception e)
+                                    {
+                                        throw e;
+                                    }
+
+                                }
+
+                            }
+
+                            driver1.Url = "https://www.ebay.com/";
+                        }
+                    }
+                    else
+                    {
+                        Message = "There are no Items in list which is greator than " + Search_sold;
+                    }
+
+                    driver.Quit();
+                    driver1.Quit();
+                }
+                else
+                {
+                    Message = "Given Products have value 0";
+                }
+
+            }catch(Exception e)
+            {
+                throw e;
+            }
+        }
+
+    }
+}
